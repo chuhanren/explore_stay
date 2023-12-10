aa2478f722c557ffbcc5273ad36f3e3e97176df1
lucklyjosh <lucklyjosh@gmail.com>
2023-12-10 14:28:51 -0500

    josh

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
 // import Modal from "./components/modals/Modal";
-import RegisterModal from "./components/modals/RegisterModal";
+// import RegisterModal from "./components/modals/RegisterModal";
 import ToasterProvider from "./providers/ToasterProvider";
 
 export const metadata: Metadata = {
@@ -26,7 +26,7 @@ export default function RootLayout({
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider/>
-          <RegisterModal />
+          {/* <RegisterModal /> */}
           <Navbar />
         </ClientOnly>
         {children}
